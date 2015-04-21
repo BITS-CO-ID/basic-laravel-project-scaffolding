e140ce7fcc4406fa19f9ab7ad05a3bd24620eaba
Ryan Kent <hello@interphased.com>
2015-04-21 18:52:14 -0400

    update asset urls

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link href="{{ asset('/assets/css/application.css') }}" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,7 +53,7 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="{{ asset('/assets/js/vendor.js') }}"></script>
+	<script src="{{ asset('/assets/js/application.js') }}"></script>
 </body>
 </html>
